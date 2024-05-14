58c3e32d646ce0b27278a3663e05d26d60bb818d
Henrique Marques <henriquemarques@Henriques-Mini.fritz.box>
2024-05-14 13:22:56 +0200

    add asAuth middleware

---
+++ dist/middleware/is-auth.js
@@ -0,0 +1,25 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.isAuth = void 0;
+const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
+const error_handling_1 = require("../util/error-handling");
+const isAuth = (req, res, next) => {
+    const token = req.get("Authorization").split(" ")[1];
+    let decodedToken;
+    try {
+        decodedToken = jsonwebtoken_1.default.verify(token, "someLongSecretString");
+    }
+    catch (error) {
+        throw error;
+    }
+    if (!decodedToken) {
+        const error = new error_handling_1.CustomError("Not authenticated", 401);
+        throw error;
+    }
+    req.userId = decodedToken.userId;
+    next();
+};
+exports.isAuth = isAuth;
---
+++ src/middleware/is-auth.ts
@@ -0,0 +1,28 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import { JwtPayload } from "jsonwebtoken";
+import { CustomError } from "../util/error-handling";
+
+declare global {
+  namespace Express {
+    interface Request {
+      userId?: string;
+    }
+  }
+}
+
+export const isAuth = (req: Request, res: Response, next: NextFunction) => {
+  const token = req.get("Authorization")!.split(" ")[1];
+  let decodedToken: JwtPayload;
+  try {
+    decodedToken = jwt.verify(token, "someLongSecretString") as JwtPayload;
+  } catch (error) {
+    throw error;
+  }
+  if (!decodedToken) {
+    const error = new CustomError("Not authenticated", 401);
+    throw error;
+  }
+  req.userId = decodedToken.userId;
+  next();
+};
