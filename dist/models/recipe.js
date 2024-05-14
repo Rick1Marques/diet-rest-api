76a3c780ddcdf038ad7ccc7e466b6b6a2217f735
Henrique Marques <henriquemarques@Henriques-Mini.fritz.box>
2024-05-14 13:47:47 +0200

    add recipe model

---
+++ dist/models/recipe.js
@@ -0,0 +1,57 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose_1 = __importDefault(require("mongoose"));
+const mongoose_2 = require("mongoose");
+const recipeSchema = new mongoose_2.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    category: {
+        type: String,
+        required: true,
+    },
+    ingredients: [
+        {
+            name: { type: String, required: true },
+            quantity: { type: Number, required: true },
+            unit: { type: String, required: true },
+        },
+    ],
+    vegetarian: {
+        type: Boolean,
+    },
+    vegan: {
+        type: Boolean,
+    },
+    nutrition: {
+        calories: {
+            type: Number,
+            required: true,
+        },
+        carbohydrate: {
+            type: Number,
+            required: true,
+        },
+        protein: {
+            type: Number,
+            required: true,
+        },
+        fat: {
+            type: Number,
+            required: true,
+        },
+    },
+    instructions: {
+        type: String,
+        required: true,
+    },
+    preparationTime: {
+        type: Number,
+        required: true,
+    },
+});
+exports.default = mongoose_1.default.model("Recipe", recipeSchema);
